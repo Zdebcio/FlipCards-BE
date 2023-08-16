a9ef36f69ef97409b67b43ebcd3688f92cb71adb
Adam Zdebel <adam.zdebel1997@gmail.com>
2023-08-16 23:49:52 +0200

    remove unused code

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -6,26 +6,6 @@ const auth = new AuthController();
 
 router.post('/login', auth.login);
 
-/**
- * @openapi
- * /user/register:
- *      post:
- *          description: Create new user account
- *          parameters:
- *              schema
- *          responses:
- *              201:
- *                  description: Returns a mysterious string.
- *                  content:
- *                      application/json:
- *                          schema:
- *                              email: string
- *                              password: string
- *              409:
- *                  description: User already exist.
- *              422:
- *                  description: Validation error.
- */
 router.post('/register', auth.register);
 
 export default router;

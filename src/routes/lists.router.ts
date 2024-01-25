e60271c559b88962b4ffe7ba9f094f2ea8646f8e
Adam Zdebel <adam.zdebel1997@gmail.com>
2024-01-25 21:47:27 +0100

    FC-33 create endpoint for Get List (#15)
    * feat: Create endpoint to get single list data
    
    * feat: Create swagger for Get List data

--- src/routes/lists.router.ts
+++ src/routes/lists.router.ts
@@ -7,5 +7,6 @@ const lists = new ListsController();
 router.post('/create', lists.create);
 
 router.get('/user-lists', lists.getUserLists);
+router.get('/:listID', lists.getList);
 
 export default router;
